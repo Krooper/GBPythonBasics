99ba235a1fe216c09a64b895f5847c937755cc54
Krooper <98321262+Krooper@users.noreply.github.com>
2022-11-27 04:31:36 +0300

    made 3rd task and added smth in 2nd task

--- 2nd.py
+++ 2nd.py
@@ -49,34 +49,74 @@ def nums_interval():
 def two_max_nums():
     # Вводим длину списка:
     list_length = is_number_and_int(input(f'Введите кол-во чисел: '))
+    if list_length == 0:
+        return
 
     # Зполняем список:
-    num_list = []
-    for i in range(int(list_length)):
-        print(f"Введите {i + 1}-й элемент массива")
-        num_list.append(is_number_and_int(input()))
+    # num_list = []
+    # for i in range(int(list_length)):
+    #     print(f"Введите {i + 1}-й элемент массива")
+    #     num_list.append(is_number_and_int(input()))
 
     # Далее можно было использовать сортировки и вывести 2 наибольших числа,
     # но это бы значительно повысило сложность (O(N^2) для сортировки пузырьком или O(N log N) для sort()),
     # поэтому решил, что лучше обойтись без сортировки, так как она тут будет избыточна
 
     # Это, однако, заставляет использовать некоторый костыль, а именно - ограничиваться диапазоном int-ых чисел:
-    max1 = -2147483648
-    max2 = -2147483648
-    for i in range(len(num_list)):
-        if num_list[i] >= max1 and num_list[i] >= max2:
+    # max1, max2 = -2147483648, -2147483648
+    # for i in range(len(num_list)):
+    #     if num_list[i] >= max1 and num_list[i] >= max2:
+    #         max2 = max1
+    #         max1 = num_list[i]
+    #     elif max1 >= num_list[i] >= max2:
+    #         max2 = num_list[i]
+
+    # В принципе в целях экономии ресурсов (понижение сложности и отсутсвие списка),
+    # можно вообще обойтись без списка и сделать всё в одном цикле:
+
+    max1, max2 = -2147483647, -2147483648
+    for i in range(int(list_length)):
+        num = is_number_and_int(input(f"Введите {i + 1}-е число: "))
+        if num >= max1 and num >= max2:
             max2 = max1
-            max1 = num_list[i]
-        elif max1 >= num_list[i] >= max2:
-            max2 = num_list[i]
+            max1 = num
+        elif max1 >= num >= max2:
+            max2 = num
 
-    print(max1, max2)
-
-
+    print(f"2 максимальных числа: {max1, max2}")
 
 
+# 3) Вводим с клавиатуры целое число - это зарплата.
+# Нужно вывести в консоль -  Минимальное кол-во  купюр, которыми можно выдать ЗП.
+# И сколько, и каких бухгалтер выдаст 25 рублевых купюр,  10 рублевых, 3 рублевых и 1 рублевых
 
+def salary_bill_calc():
+    # Вводим зарплату:
+    salary = is_number_and_int(input(f'Введите зарплату: '))
+
+    # Создаем счетчики для купюр:
+    count_25, count_10, count_3, count_1 = 0, 0, 0, 0
+
+    # Начинаем считать:
+    count_25 = salary // 25
+    salary %= 25
+
+    count_10 = salary // 10
+    salary %= 10
+
+    count_3 = salary // 3
+    salary %= 3
+
+    count_1 = salary // 1
+    salary %= 1
+
+    print(f'Минимальное кол-во купюр: {count_25+count_10+count_3+count_1}')
+    print(f'Кол-во купюр номиналом 25 руб: {count_25}')
+    print(f'Кол-во купюр номиналом 10 руб: {count_10}')
+    print(f'Кол-во купюр номиналом 3 руб: {count_3}')
+    print(f'Кол-во купюр номиналом 1 руб: {count_1}')
 
 
 #nums_interval()
-two_max_nums()
+#two_max_nums()
+salary_bill_calc()
