d49933aaaecd3112f4c7102ef51bc4353a6f68f9
Krooper <98321262+Krooper@users.noreply.github.com>
2022-11-27 04:10:09 +0300

    made 2nd task

--- 2nd.py
+++ 2nd.py
@@ -1,10 +1,10 @@
 def is_number_and_int(number):
     try:
         int(number)
-        return True
+        return int(number)
     except ValueError:
-        print("Вы ввели не число или число не целое!")
-        return False
+        print("Вы ввели не число или число не целое! Повторите ввод!")
+        return is_number_and_int(input())
 
 
 def is_integer(number):
@@ -23,20 +23,17 @@ def positive(number):
         return number
 
 
-## 1) Вводим с клавиатуры целое число X и У.
-## Выводим на экран количество чисел между Х и У, которые делятся на 2 и 3
+# 1) Вводим с клавиатуры целое число X и У.
+# Выводим на экран количество чисел между Х и У, которые делятся на 2 и 3
 
 def nums_interval():
-    lower_num = input(f'Введите начало диапазона: ')
-    while not is_number_and_int(lower_num):
-        lower_num = input(f'Введите начало диапазона: ')
-    lower_num = int(lower_num) + 1
+    # Вводим первое число:
+    lower_num = is_number_and_int(input(f'Введите начало диапазона: ')) + 1
 
-    higher_num = input(f'Введите конец диапазона: ')
-    while not is_number_and_int(higher_num):
-        higher_num = input(f'Введите начало диапазона: ')
-    higher_num = int(higher_num)
+    # Вводим второе число:
+    higher_num = is_number_and_int(input(f'Введите конец диапазона: '))
 
+    # Считаем числа, которые делятся на 2 или 3 без остатка:
     counter = 0
     for num in range(lower_num, higher_num):
         if num % 2 == 0 or num % 3 == 0:
@@ -45,4 +42,41 @@ def nums_interval():
     print(counter)
 
 
-nums_interval()
+# 2) Вводим с клавиатуры целое число X
+# Далее вводим Х целых чисел.
+# Необходимо вывести на экран максимальное и второе максимальное число из введенных чисел.
+
+def two_max_nums():
+    # Вводим длину списка:
+    list_length = is_number_and_int(input(f'Введите кол-во чисел: '))
+
+    # Зполняем список:
+    num_list = []
+    for i in range(int(list_length)):
+        print(f"Введите {i + 1}-й элемент массива")
+        num_list.append(is_number_and_int(input()))
+
+    # Далее можно было использовать сортировки и вывести 2 наибольших числа,
+    # но это бы значительно повысило сложность (O(N^2) для сортировки пузырьком или O(N log N) для sort()),
+    # поэтому решил, что лучше обойтись без сортировки, так как она тут будет избыточна
+
+    # Это, однако, заставляет использовать некоторый костыль, а именно - ограничиваться диапазоном int-ых чисел:
+    max1 = -2147483648
+    max2 = -2147483648
+    for i in range(len(num_list)):
+        if num_list[i] >= max1 and num_list[i] >= max2:
+            max2 = max1
+            max1 = num_list[i]
+        elif max1 >= num_list[i] >= max2:
+            max2 = num_list[i]
+
+    print(max1, max2)
+
+
+
+
+
+
+
+#nums_interval()
+two_max_nums()
