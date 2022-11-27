eb61307bc1b98d7dff1aec5adb9281025bba641f
Krooper <98321262+Krooper@users.noreply.github.com>
2022-11-27 03:25:41 +0300

    made 1st task

--- 2nd.py
+++ 2nd.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def is_number_and_int(number):
+    try:
+        int(number)
+        return True
+    except ValueError:
+        print("Вы ввели не число или число не целое!")
+        return False
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def is_integer(number):
+    if number.isdigit():
+        return True
+    else:
+        print("Вы ввели дробное число!")
+        return False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def positive(number):
+    if number[0] == '-':
+        print("Отрицательное число преобразовано в положительное.")
+        return number[1:]
+    else:
+        return number
+
+
+## 1) Вводим с клавиатуры целое число X и У.
+## Выводим на экран количество чисел между Х и У, которые делятся на 2 и 3
+
+def nums_interval():
+    lower_num = input(f'Введите начало диапазона: ')
+    while not is_number_and_int(lower_num):
+        lower_num = input(f'Введите начало диапазона: ')
+    lower_num = int(lower_num) + 1
+
+    higher_num = input(f'Введите конец диапазона: ')
+    while not is_number_and_int(higher_num):
+        higher_num = input(f'Введите начало диапазона: ')
+    higher_num = int(higher_num)
+
+    counter = 0
+    for num in range(lower_num, higher_num):
+        if num % 2 == 0 or num % 3 == 0:
+            counter += 1
+
+    print(counter)
+
+
+nums_interval()
