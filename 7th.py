3575deebcc558849b6db2af8c0e0fc252b625a4b
Krooper <98321262+Krooper@users.noreply.github.com>
2022-12-15 14:32:16 +0300

    Сделал функцию добавления события

--- 7th.py
+++ 7th.py
@@ -6,6 +6,7 @@
 вывести, показать, перезаписать, добавить событие в определенный день недели
 """
 import time
+import os
 
 
 # Функция для создания файла со словарем расшифровки дней недели
@@ -71,7 +72,6 @@ def time_getter(inp_time):
 # Функция собирает время и день недели, делает из них строку
 # Возвращает эту строку (будет использоваться дальше как ключ другого словаря)
 # Вторым элементом кортежа возвращает заполненную этим днем недели и временем структуру time
-# Структура будет использоваться в дальнейшем для сравнения
 def day_and_time_getter():
     inp_day = day_getter(input(f'Введите номер дня недели:\n"1" - Понедельник\n"2" - Вторник\n"3" - Среда'
                                f'\n"4" - Четверг\n"5" - Пятница\n"6" - Суббота\n"7" - Воскресенье\n'))
@@ -82,7 +82,7 @@ def day_and_time_getter():
     week_day_str = week_days_dict[str(day_and_time_struct.tm_wday)]
     time_str = time.strftime("%H:%M", day_and_time_struct)
 
-    return f'{week_day_str}, {time_str}', day_and_time_struct
+    return f'{week_day_str}, {time_str}'
 
 
 # Функция для создания пустого расписания
@@ -107,7 +107,7 @@ def empty_schedule_generator():
 def save_schedule(schedule):
     with open("schedule.txt", mode='w', encoding='utf-8') as schedule_file:
         for day_and_time, activity in schedule.items():
-            schedule_file.write('{}:{}\n'.format(day_and_time, activity))
+            schedule_file.write('{} - {}\n'.format(day_and_time, activity))
 
     return schedule_file.name
 
@@ -117,21 +117,10 @@ def get_schedule(file='schedule.txt'):
     schedule = {}
     with open(file, mode='r', encoding='utf-8') as schedule_file:
         for i in schedule_file.readlines():
-            day_and_time, activity = i.strip().split(':')
+            day_and_time, activity = i.strip().split(' - ')
             schedule[day_and_time] = activity
 
     return schedule
-
-
-# Функция для получения текущего расписания из файла
-def get_active_schedule(file='active_schedule.txt'):
-    active_schedule = {}
-    with open(file, mode='r', encoding='utf-8') as active_schedule_file:
-        for i in active_schedule_file.readlines():
-            day_and_time, activity = i.strip().split(':')
-            active_schedule[day_and_time] = activity
-
-    return active_schedule
 
 
 # Функция для получения текущего расписания из общего
@@ -148,48 +137,74 @@ def get_active_schedule_first(schedule):
     return active_schedule
 
 
+# Функция для получения текущего расписания из файла
+def get_active_schedule():
+    active_schedule = {}
+    with open('active_schedule.txt', mode='r', encoding='utf-8') as active_schedule_file:
+        for i in active_schedule_file.readlines():
+            day_and_time, activity = i.strip().split(' - ')
+            active_schedule[day_and_time] = activity
+
+    return active_schedule
+
+
 # Функция для сохранения текущего расписания в файл
 def save_active_schedule(active_schedule):
     with open("active_schedule.txt", mode='w', encoding='utf-8') as active_schedule_file:
         for day_and_time, activity in active_schedule.items():
-            active_schedule_file.write('{}:{}\n'.format(day_and_time, activity))
+            active_schedule_file.write('{} - {}\n'.format(day_and_time, activity))
 
     return active_schedule_file.name
 
 
 # Функция для вывода текущего расписания в консоль
-def print_active_schedule(schedule):
-    active_schedule_file = save_active_schedule(get_active_schedule(schedule))
-    active_schedule = get_active_schedule(schedule)
+def print_active_schedule():
+    active_schedule_file = save_active_schedule(get_active_schedule())
+    active_schedule = get_active_schedule()
     print(f'Ваше расписание записано в файл {active_schedule_file}')
     print('Вот ваше расписание:')
-    for activity in active_schedule:
-        print(activity)
+    for day_and_time, activity in active_schedule.items():
+        print(f'{day_and_time} - {activity}')
 
 
 # Функция для добавления события в расписание
 def add():
+    inp_day_and_time = day_and_time_getter()
+    active_schedule = get_active_schedule()
+    if inp_day_and_time in active_schedule.keys():
+        print('Это время уже занято!')
+        return
+    else:
+        activity = input(f'Событие: ')
+        active_schedule[inp_day_and_time] = activity
+        save_active_schedule(active_schedule)
 
-    return schedule
+        schedule = get_schedule()
+        schedule[inp_day_and_time] = activity
+        save_schedule(schedule)
+
+        print('Успешно!')
+    return
 
 
-# Функция для изменения события в расписании
-def rewrite():
-
-    return schedule
-
-
-# Функция для удаления события из расписания
-def delete():
-
-    return schedule
+# # Функция для изменения события в расписании
+# def rewrite():
+#
+#     return schedule
+#
+#
+# # Функция для удаления события из расписания
+# def delete():
+#
+#     return schedule
 
 
 # Функция для выбора действия, которое хочет совершить пользователь
 # Является основной функцией - зациклена и имеет возможность выхода
 def input_command():
-
-    save_active_schedule(get_active_schedule_first(get_schedule(save_schedule(empty_schedule_generator()))))
+    weekdays_file_creator()
+    if not os.path.exists('active_schedule.txt'):
+        save_active_schedule(get_active_schedule_first(get_schedule(save_schedule(empty_schedule_generator()))))
 
     while True:
         inp_com = input(f'Введите номер действия:\n"1" - Показать расписание\n'
@@ -198,16 +213,16 @@ def input_command():
         try:
             inp_com = int(inp_com)
             if inp_com == 1:
-                print_active_schedule(get_active_schedule_first(
-                    get_schedule(save_schedule(empty_schedule_generator()))))
+                save_active_schedule(get_active_schedule_first(get_schedule()))
+                print_active_schedule()
             elif inp_com == 2:
-
-            elif inp_com == 3:
-                new_schedule = rewrite()
-                schedule = schedule_file_saver(new_schedule)
-            elif inp_com == 4:
-                new_schedule = delete()
-                schedule = schedule_file_saver(new_schedule)
+                add()
+            # elif inp_com == 3:
+            #     new_schedule = rewrite()
+            #     schedule = schedule_file_saver(new_schedule)
+            # elif inp_com == 4:
+            #     new_schedule = delete()
+            #     schedule = schedule_file_saver(new_schedule)
             elif inp_com == 0:
                 quit()
             else:
@@ -215,3 +230,6 @@ def input_command():
         except ValueError:
             print('Нет такой команды, попробуйте еще раз!')
             return input_command()
+
+
+input_command()
